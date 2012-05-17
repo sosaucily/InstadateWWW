9ffa4468058a32ea39b970625eba44f96fc9ce7f
Jesse Smith <jesses16@gmail.com>
2012-05-16 17:29:59 -0700

    Modify access to instadateapp url

--- javascripts/loadconfig.js
+++ javascripts/loadconfig.js
@@ -1,7 +1,7 @@
 if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/)) {
   //alert ("Device!");
   window.instadateConfig = {
-  	data_server_url:"http://www.instadateapp.com/"
+  	data_server_url:document.baseURI
   }
 } else {
   //alert ("Desktop Browser!"); //this is the browser
