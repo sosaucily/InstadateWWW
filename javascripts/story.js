d5426cd8d9c4a2ccd5ea69d1f33d4aacd22835eb
Jesse Smith <jesses16@gmail.com>
2012-05-27 22:48:36 -0700

    Updated background images, business info

--- javascripts/story.js
+++ javascripts/story.js
@@ -28,14 +28,33 @@ initialize_story = function(options) {
 		if (curr_chap_data.image_url == null) {
 			curr_chap_data.image_url = document.baseURI + "/images/default-activity-icon.png";
 		}
+		
+		click_phone = ""
+		display_phone = ""
+		try {
+			if (curr_chap_data.phone != null ) {
+				click_phone = curr_chap_data.phone.replace(/-/g,"").substr(2,curr_chap_data.phone.length-2);
+				display_phone = "(" + click_phone.substring(0,3) + ") " + click_phone.substring(3,6) + "-" + click_phone.substring(6,10)
+			}
+		}
+		catch(err){
+			click_phone = "";
+			display_phone = "";
+		}
 
-		click_phone = curr_chap_data.phone.replace(/-/g,"").substr(2,curr_chap_data.phone.length-2);
+		display_address = curr_chap_data.address + ", " + curr_chap_data.city;
+		display_address = display_address.substring(0,35); 
 
-		map_url = "http://maps.google.com/maps?q=" +curr_chap_data.latitude+","+curr_chap_data.longitude
+		map_url = "http://maps.google.com/maps?q=" + curr_chap_data.latitude + "," + curr_chap_data.longitude
 
 		img_html = "<img src=\"" + curr_chap_data.image_url + "\" alt=\"pic\"></img>";
 		Story.chapter_html[chap] = "<div id=\"" + Story.chapter_ids[chap] + "\" class=\"activity activity-" + curr_chap_data.category + "-evening\"><div class=\"chapter ui-corner-all\" ><table><tr><td><span id=\"" + curr_id + "_img\">" + img_html + "</span></td><td columnwidth=\"2\"><span id=\"" + curr_id + "\"><div class=\"meta\" ><div class=\"chapter-header\"><strong>Chapter " + (chap+1) + ": " + curr_chap_data.category + "</strong></div><div class=\"chapter-details\"><strong>" + curr_chap_data.name + "</strong><br/>" + curr_chap_data.source_category[0] + "</div></div></span></td></tr></table></div></div>";
-		Story.chapter_details_html[chap] =   "<div id=\"" + curr_id + "_details\" style=\"display:none;margin: 10px 10px 10px 10px\">	<!-- Map -->    <div class=\"map_container\" style=\"margin-bottom: 10px;\">        <div class=\"the_map\" id=\"themap" + (chap+1) + "\"></div>    </div><!--END OF: Map -->	<!-- Address and Phone -->    <ul data-role=\"listview\" data-theme=\"a\" data-inset=\"true\">        <li><img src=\"images/pin.png\" alt=\"Location\" class=\"ui-li-icon\"><span id=\"" + curr_id + "_addr\"><a href=\"" + map_url + "\">" + curr_chap_data.address + ", " + curr_chap_data.city + "</a></span></li>        <li><img src=\"images/phone.png\" alt=\"Phone\" class=\"ui-li-icon\" data-mini=\"true\"><span id=\"" + curr_id + "_phone\"><a href=\"tel:" +click_phone+ "\">" + curr_chap_data.phone + "</a></span></li>    </ul>  <!-- END OF: Address and Phone --></div>";
+		Story.chapter_details_html[chap] =   "<div id=\"" + curr_id + "_details\" style=\"display:none;margin: 10px 10px 10px 10px\">	<!-- Map -->    <div class=\"map_container\" style=\"margin-bottom: 10px;\">        <div class=\"the_map\" id=\"themap" + (chap+1) + "\"></div>    </div><!--END OF: Map -->	<!-- Address and Phone -->";
+		Story.chapter_details_html[chap] += "<ul data-role=\"listview\" data-theme=\"a\" data-mini=\"true\" data-inset=\"true\">";
+		Story.chapter_details_html[chap] += "<li><img src=\"images/pin.png\" alt=\"Location\" class=\"ui-li-icon\"><span id=\"" + curr_id + "_addr\"><a class=\"addr_link\" href=\"" + map_url + "\" target=\"_blank\">" + display_address + "</a></span></li>";
+		Story.chapter_details_html[chap] += "<li><img src=\"images/phone.png\" alt=\"Phone\" class=\"ui-li-icon\"><span id=\"" + curr_id + "_phone\"><a class=\"phone_link\" href=\"tel:" +click_phone+ "\">" + display_phone + "</a></span></li>";
+		Story.chapter_details_html[chap] += "<li><img src=\"images/info.png\" alt=\"Yelp\" class=\"ui-li-icon\"><span id=\"" + curr_id + "_api\"><a class=\"api_link\" href=\"" +curr_chap_data.business_url+ "\">More details</a></span><div style=\"margin:0 0 0 10px;display:inline-block;\"><a href=\"" +curr_chap_data.business_url+ "\"><img src=\"images/yelp_logo_30.png\" style=\"margin:-6px 0 -10px 0px;\" /></a></div></li>";
+		Story.chapter_details_html[chap] += "</ul>  <!-- END OF: Address and Phone --></div>";
 		$('#story_list').append(Story.chapter_html[chap]);
 		$('#story_list').append(Story.chapter_details_html[chap]);
 		$('#' + curr_id + "_details").trigger("create");
@@ -84,7 +103,7 @@ function getMap(lat,lng, elementid)
 	        };
     var map = new google.maps.Map(document.getElementById(elementid), myOptions);
 	*/
-	return ("http://maps.googleapis.com/maps/api/staticmap?center=" + lat + "," + lng + "&zoom=15&markers=color:blue%7Clabel:S%7C" + lat + "," + lng + "&size=288x100&sensor=false");
+	return ("http://maps.googleapis.com/maps/api/staticmap?center=" + lat + "," + lng + "&zoom=15&markers=color:blue%7Clabel:S%7C" + lat + "," + lng + "&size=288x87&sensor=false");
 }
 
 function collapse_all_but(chapter_elem)
@@ -135,7 +154,7 @@ $(function() {
 		if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/)) {
 			if (window.instadateConfig["data_server_url"].match(/InstadateIphone/)) {
 				//Changing the backend URL because we're on an iphone or iphone simulator
-				window.instadateConfig["data_server_url"] = "http://192.168.0.16:9292/";
+				window.instadateConfig["data_server_url"] = "http://192.168.1.73:9292/";
 			}
 		}
 		console.log ("Getting data from backend url: " + window.instadateConfig["data_server_url"] + "story/create" );
