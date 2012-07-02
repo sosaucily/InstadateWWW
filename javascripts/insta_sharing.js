67aecc32ee7e8b661bb24c52d911643bea209ce9
Jesse Smith <jesses16@gmail.com>
2012-07-02 14:39:31 -0700

    changes to sharing functions

--- javascripts/insta_sharing.js
+++ javascripts/insta_sharing.js
@@ -1,18 +1,27 @@
 function authAndShareToFacebook(isLoggedIn, message, url) {
 	if (isLoggedIn) {
-		window.plugins.nativeControls.facebookConnect();
-		window.plugins.nativeControls.shareToFacebook(message, url);
+		window.plugins.shareKit.facebookConnect();
+		window.plugins.shareKit.shareToFacebook(message, url);
 	}
 	else {
-		window.plugins.nativeControls.shareToFacebook(message, url);
+		window.plugins.shareKit.shareToFacebook(message, url);
+	}
+}
+
+function authAndShareToTwitter(isLoggedIn, message, url) {
+	if (isLoggedIn) {
+		window.plugins.shareKit.twitterConnect();
+		window.plugins.shareKit.shareToTwitter(message, url);
+	}
+	else {
+		window.plugins.shareKit.shareToTwitter(message, url);
 	}
 }
 
 function showSharingSheet() {
 	console.log("Running showSharingSheet function");
 	var buttons = ["Share to Facebook", "Share to Twitter", "Share via Email", "Cancel"];
-	nativeControls = window.plugins.nativeControls;
-	var delegate = nativeControls.createActionSheet(buttons, null, 3, 3);
+	var delegate = window.plugins.nativeControls.createActionSheet(buttons, null, 3, 3);
 	var message = "Test message from Instadate";
 	var url = "http://www.instadateapp.com/testtest";
 	var subject = "Check out this date!";
@@ -22,17 +31,18 @@ function showSharingSheet() {
 		switch(index) {
 			case 0:
 				console.log("Sharing to Facebook!");
-				nativeControls.isLoggedToFacebook(function(isLoggedIn) { authAndShareToFacebook(isLoggedIn, message, url); });
+				window.plugins.shareKit.isLoggedToFacebook(function(isLoggedIn) { authAndShareToFacebook(isLoggedIn, message, url); });
 				console.log("Done");
 				break;
 			case 1:
 				console.log("Sharing to Twitter!");
-				nativeControls.shareToTwitter(message, url);
+				window.plugins.shareKit.isLoggedToTwitter(function(isLoggedIn) { authAndShareToTwitter(isLoggedIn, message, url); });
+				window.plugins.shareKit.shareToTwitter(message, url);
 				console.log("Done");
 				break;
 			case 2:
 				console.log("Sharing via Email!");
-				nativeControls.shareToMail(subject, body);
+				window.plugins.shareKit.shareToMail(subject, body);
 				console.log("Done");
 				break;
 			default:
