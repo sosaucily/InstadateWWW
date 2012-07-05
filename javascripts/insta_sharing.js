4b96b57b98fc32f7514b6e2969ef6d1ef7c44c5c
Jesse Smith <jesses16@gmail.com>
2012-07-05 11:09:39 -0700

    Updated sharing functionality and content

--- javascripts/insta_sharing.js
+++ javascripts/insta_sharing.js
@@ -1,24 +1,24 @@
 function authAndShareToFacebook(isLoggedIn, message, url) {
 	if (isLoggedIn) {
 		window.plugins.shareKit.facebookConnect();
-		window.plugins.shareKit.shareToFacebook(message + "http://theoyster.me/stories/" + Story.current_story.story_id, url);
+		window.plugins.shareKit.shareToFacebook(message + "http://" + url, null);
 	}
 	else {
-		window.plugins.shareKit.shareToFacebook(message + "http://theoyster.me/stories/" + Story.current_story.story_id, url);
+		window.plugins.shareKit.shareToFacebook(message + "http://" + url, null);
 	}
 }
 
 function authAndShareToTwitter(isLoggedIn, message, url) {
 	if (isLoggedIn) {
 		window.plugins.shareKit.twitterConnect();
-		window.plugins.shareKit.shareToTwitter(message + "theoyster.me/stories/" + Story.current_story.story_id, url);
+		window.plugins.shareKit.shareToTwitter(message + url, null);
 	}
 	else {
-		window.plugins.shareKit.shareToTwitter(message + "theoyster.me/stories/" + Story.current_story.story_id, url);
+		window.plugins.shareKit.shareToTwitter(message + url, null);
 	}
 }
 
-function showSharingSheet(message, subject, body) {
+function showSharingSheet(message, subject, body, url) {
 	console.log("Running showSharingSheet function");
 	var buttons = ["Share to Facebook", "Share to Twitter", "Share via Email", "Cancel"];
 	var delegate = window.plugins.nativeControls.createActionSheet(buttons, null, 3, 3);
@@ -27,18 +27,17 @@ function showSharingSheet(message, subject, body) {
 		switch(index) {
 			case 0:
 				console.log("Sharing to Facebook!");
-				window.plugins.shareKit.isLoggedToFacebook(function(isLoggedIn) { authAndShareToFacebook(isLoggedIn, message, null); });
+				window.plugins.shareKit.isLoggedToFacebook(function(isLoggedIn) { authAndShareToFacebook(isLoggedIn, message, url); });
 				console.log("Done");
 				break;
 			case 1:
 				console.log("Sharing to Twitter!");
-				window.plugins.shareKit.isLoggedToTwitter(function(isLoggedIn) { authAndShareToTwitter(isLoggedIn, message, null); });
-				window.plugins.shareKit.shareToTwitter(message, url);
+				window.plugins.shareKit.isLoggedToTwitter(function(isLoggedIn) { authAndShareToTwitter(isLoggedIn, message, url); });
 				console.log("Done");
 				break;
 			case 2:
 				console.log("Sharing via Email!");
-				window.plugins.shareKit.shareToMail(subject, body);
+				window.plugins.shareKit.shareToMail(subject, body + url);
 				console.log("Done");
 				break;
 			default:
@@ -60,13 +59,15 @@ $(function() {
 	$('#share_story_screen').click(function() {
 		var message = window.search_city + " is my Oyster - ";
 		var subject = window.search_city + " is my Oyster";
-		var body = "Check out what I just planned with Oyster - " + "theoyster.me/stories/" + Story.current_story.story_id;
-		showSharingSheet(message, subject, body);
+		var body = "Check out what I just planned with Oyster - ";
+		var url = "theoyster.me/stories/" + Story.current_story.story_id;
+		showSharingSheet(message, subject, body, url);
 	});
 	$('#share_home_screen').click(function() {
 		var message = "Check out Oyster, the best way to discover new things near you - ";
 		var subject = "Check out Oyster";
-		var body = "Oyster helps you find new and interesting things to do near you. Get it now - " + "theoyster.me/stories/" + Story.current_story.story_id;
-		showSharingSheet(message, subject, body);
+		var body = "Oyster helps you find new and interesting things to do near you. Get it now - ";
+		var url = "theoyster.me";
+		showSharingSheet(message, subject, body, url);
 	});
 });
--- javascripts/story.js
+++ javascripts/story.js
@@ -133,7 +133,7 @@ function getMap(lat,lng, elementid)
 	        };
     var map = new google.maps.Map(document.getElementById(elementid), myOptions);
 	*/
-	return ("http://maps.googleapis.com/maps/api/staticmap?center=" + lat + "," + lng + "&zoom=16&markers=color:blue%7Clabel:S%7C" + lat + "," + lng + "&size=576x174&sensor=false");
+	return ("http://maps.googleapis.com/maps/api/staticmap?center=" + lat + "," + lng + "&zoom=17&markers=color:blue%7Clabel:S%7C" + lat + "," + lng + "&size=576x174&sensor=false");
 }
 
 function collapse_all_but(chapter_elem)
