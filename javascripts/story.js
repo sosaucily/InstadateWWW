56c4879cb44c916382b0d82def3baeb16d2250b3
Jesse Smith <jesses16@gmail.com>
2012-11-25 15:49:58 -0800

    update calls to google static map with api key

--- javascripts/story.js
+++ javascripts/story.js
@@ -70,7 +70,7 @@ comeBackOnline = function(){
 dynamicallyReloadGoogle = function(){
 	var fileref=document.createElement('script');
 	fileref.setAttribute("type","text/javascript");
-	fileref.setAttribute("src", "http://maps.googleapis.com/maps/api/js?key = AIzaSyCloHHOTgUAwKqVVpovHVzGioPQ8RkQeTY&sensor=false");
+	fileref.setAttribute("src", "http://maps.googleapis.com/maps/api/js?key=AIzaSyCloHHOTgUAwKqVVpovHVzGioPQ8RkQeTY&sensor=false");
 	document.getElementsByTagName("head")[0].appendChild(fileref);
 }
 
@@ -321,7 +321,7 @@ function getMap(lat,lng, elementid)
 	        };
     var map = new google.maps.Map(document.getElementById(elementid), myOptions);
 	*/
-	return ("http://maps.googleapis.com/maps/api/staticmap?center=" + lat + "," + lng + "&zoom=17&markers=color:blue%7Clabel:S%7C" + lat + "," + lng + "&size=576x174&sensor=false");
+	return ("http://maps.googleapis.com/maps/api/staticmap?center=" + lat + "," + lng + "&zoom=17&markers=color:blue%7Clabel:S%7C" + lat + "," + lng + "&size=576x174&sensor=false&key=AIzaSyCloHHOTgUAwKqVVpovHVzGioPQ8RkQeTY");
 }
 
 function custom_update_loading_image(city) {
